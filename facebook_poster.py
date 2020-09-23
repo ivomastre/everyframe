92483f137b4c2fe4deb1180d6a73872c16857950
ivomastre <ivomastre@gmail.com>
2020-09-23 07:41:12 -0300

    added add season method

--- facebook_poster.py
+++ facebook_poster.py
@@ -12,19 +12,36 @@ def save_to_json(status_data):
 
 
 def add_season_to_json():
-    return 0
+    print("minha pica")
+    with open('./status.json') as f:
+        status_data = json.load(f)
+        status_data['Season']+=1
+                    
+        episode_dir = os.listdir('./assets/frames/' )
+        print(episode_dir)
+        for i,season in enumerate(episode_dir):
+            episode_dir[i] = int(season)
+            print(season)
+        print(max(episode_dir))
+        if(max(episode_dir)<status_data['Season']):
+            save_to_json({'Season': 1})
+        
+        else:
+            save_to_json({'Season': status_data['Season']})
+
 def add_episode_to_json():
     print("tiririca")
     with open('./status.json') as f:
         status_data = json.load(f)
         status_data['Episode']+=1
-
+                   
         episode_dir = os.listdir('./assets/frames/' + str(status_data['Season']))
         for i,episode in enumerate(episode_dir):
             episode_dir[i] = int(episode)
         if(max(episode_dir)<status_data['Episode']):
-            #add_season_to_json()
+            add_season_to_json()
             save_to_json({'Episode': 1})
+            
         else:
             save_to_json({'Episode': status_data['Episode']})
         print(episode_dir)
@@ -32,8 +49,11 @@ def add_episode_to_json():
 def add_frame_to_json():
     
     with open('./status.json') as f:
+        
         status_data = json.load(f)
         status_data['Frame']+=1
+        if(status_data['Episode']==0):
+            status_data['Episode'] = 1
         #if(status_data['Frame']>)
         print(status_data)
         frame_dir = os.listdir('./assets/frames/' + str(status_data['Season']) + '/' + str(status_data['Episode']))
@@ -42,7 +62,7 @@ def add_frame_to_json():
             frame_dir[i] = int(os.path.splitext(frame)[0])
         if(max(frame_dir) < status_data['Frame']):
             add_episode_to_json()
-            save_to_json({'Frame': 1})
+            save_to_json({'Frame': 0})
         else:
             save_to_json({'Frame': status_data['Frame']})
 
@@ -57,5 +77,5 @@ def main():
         relative_path = "./assets/frames/Season 0"+ str(status_data.get('Season')) + "/"+ str(status_data.get('Episode'))+ "/"+ str(status_data.get('Frame'))+".jpg"        
     add_frame_to_json()
     
-for x in range(0,50):
+for x in range(0,300):
     main()
