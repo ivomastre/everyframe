ed51eca6511acde789e43af875d7a1b4545deeb1
ivomastre <ivomastre@gmail.com>
2020-09-23 05:23:28 -0300

    make frames finished

--- make_frames_from_video.py
+++ make_frames_from_video.py
@@ -5,14 +5,30 @@ import os
 
 
 def make_frames(season, input_dir, output_dir, name):
-    #
+    #Create necessary folders
     if not os.path.exists(os.path.abspath(output_dir)):
         print(os.path.abspath(output_dir))
         os.makedirs(os.path.abspath(output_dir))
+
+    vidcap = cv2.VideoCapture(input_dir)
+    success, image = vidcap.read()
+    fps = vidcap.get(cv2.CAP_PROP_FPS)
+    required_fps = 2
+    #More fps = more frames
+    multiplier = round(fps/required_fps)
+    frame_number = 1
+    """
+    while success:
+        frame_id = int(round(vidcap.get(1)))
+        if frameId % multiplier == 0:
+            frame_number+=1
+            cv2.imwrite(os.path.join(), image)
+    """
+    vidcap.release()
+
         
-    
+        
 def main():
-    
     #Get all files into a array
     for seasons in os.listdir('./assets/video'):
         episode = os.listdir('./assets/video/'+ seasons)[0]
