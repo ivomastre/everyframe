2bdb1ae4e915dda261b037b3711c183a892b6c24
ivomastre <ivomastre@gmail.com>
2020-09-23 05:48:42 -0300

    finished frame grabber

--- make_frames_from_video.py
+++ make_frames_from_video.py
@@ -1,40 +1,39 @@
+import os
 import cv2
 
-import os
 
 
 
-def make_frames(season, input_dir, output_dir, name):
+def make_frames(input_dir, output_dir):
     #Create necessary folders
     if not os.path.exists(os.path.abspath(output_dir)):
         print(os.path.abspath(output_dir))
         os.makedirs(os.path.abspath(output_dir))
-
     vidcap = cv2.VideoCapture(input_dir)
+    print(input_dir)
     success, image = vidcap.read()
     fps = vidcap.get(cv2.CAP_PROP_FPS)
-    required_fps = 2
+    required_fps = 3
     #More fps = more frames
     multiplier = round(fps/required_fps)
     frame_number = 1
-    """
+
     while success:
         frame_id = int(round(vidcap.get(1)))
-        if frameId % multiplier == 0:
-            frame_number+=1
-            cv2.imwrite(os.path.join(), image)
-    """
-    vidcap.release()
+        success, image = vidcap.read()
 
-        
-        
+        if frame_id % multiplier == 0:
+                  
+            cv2.imwrite(os.path.join(output_dir, str(frame_number) + ".jpg"), image)
+            frame_number+=1
+
+    vidcap.release()     
 def main():
     #Get all files into a array
-    for seasons in os.listdir('./assets/video'):
+    for seasons in os.listdir('./assets/video/'):
         episode = os.listdir('./assets/video/'+ seasons)[0]
         name = os.path.splitext(episode)[0]
-        input_dir = os.path.abspath(episode)
+        input_dir = os.path.abspath('./assets/video/'+ seasons+ '/'+ episode)
         output_dir = "./assets/frames/" + seasons + "/" + name
-        print(output_dir)
-        make_frames(seasons, input_dir, output_dir, name)
+        make_frames(input_dir, output_dir)
 main()
